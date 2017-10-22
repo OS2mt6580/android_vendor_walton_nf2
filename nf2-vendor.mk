b8376d4efa2c78c105297c82961a4f107bc838db
Parthib <sirajammunirparthi24@gmail.com>
2017-10-22 19:18:11 +0600

    nf2: port

---
+++ nf2-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/walton/nf2/xbin,system/xbin)
+
